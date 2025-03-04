e426580d061ae3b722d06c5d5074b5245190f070
Алексей Цапаев <aleksei.tsapaev@yasno.live>
2025-03-04 23:38:26 +0300

    er

--- resources/ts/admin/shared/components/TreeView/TreeView.vue
+++ resources/ts/admin/shared/components/TreeView/TreeView.vue
@@ -1,28 +1,13 @@
 <template>
-    <div class="tree-view-container">
-        <v-text-field
-            v-model="search"
-            density="compact"
-            placeholder="Поиск"
-            prepend-inner-icon="mdi-magnify"
-            clearable
-            hide-details
-            @click:clear="search = ''"
-        />
+  <div class="tree-view-container">
+    <v-text-field v-model="search" density="compact" placeholder="Поиск" prepend-inner-icon="mdi-magnify"
+      class="tree-view__search" clearable hide-details @click:clear="search = ''" />
 
-        <v-list density="compact" class="tree-view">
-            <TreeViewItem
-                v-for="item in filteredItems"
-                :key="item.id"
-                :item="item"
-                :depth="0"
-                :search="search"
-                :getItemValue="getItemValue"
-                :getItemTitle="getItemTitle"
-                :onItemClick="onItemClick"
-            />
-        </v-list>
-    </div>
+    <v-list density="compact" class="tree-view">
+      <TreeViewItem v-for="item in filteredItems" :key="item.id" :item="item" :depth="0" :search="search"
+        :getItemValue="getItemValue" :getItemTitle="getItemTitle" :onItemClick="onItemClick" />
+    </v-list>
+  </div>
 </template>
 
 <script setup lang="ts" generic="T extends IBaseEntity & INestedSet<T>">
@@ -33,7 +18,7 @@ import { useItems } from "../../composables";
 
 const { items = [], itemTitle, itemValue } = defineProps<ITreeViewProps<T>>();
 const emit = defineEmits<{
-    "item:click": [value: T];
+  "item:click": [value: T];
 }>();
 
 const { getItemValue, getItemTitle } = useItems<T>({ itemTitle, itemValue });
@@ -41,27 +26,32 @@ const { getItemValue, getItemTitle } = useItems<T>({ itemTitle, itemValue });
 const search = ref("");
 
 const filteredItems = computed(() => {
-    if (!search.value) return items;
-    return filterItems(items, search.value.toLowerCase());
+  if (!search.value) return items;
+  return filterItems(items, search.value.toLowerCase());
 });
 
 function filterItems(items: T[], query: string): T[] {
-    return items
-        .map((item) => ({ ...item }))
-        .filter((item) => {
-            const matches = checkItemMatches(item, query);
-            if (item.children) {
-                item.children = filterItems(item.children, query);
-            }
-            return matches || (item.children && item.children.length > 0);
-        });
+  return items
+    .map((item) => ({ ...item }))
+    .filter((item) => {
+      const matches = checkItemMatches(item, query);
+      if (item.children) {
+        item.children = filterItems(item.children, query);
+      }
+      return matches || (item.children && item.children.length > 0);
+    });
 }
 
 function checkItemMatches(item: T, query: string): boolean {
-    return getItemTitle(item).toLowerCase().includes(query);
+  return getItemTitle(item).toLowerCase().includes(query);
 }
 
 const onItemClick = (item: T) => {
-    emit("item:click", item);
+  emit("item:click", item);
 };
 </script>
+<style lang="scss" scoped>
+.tree-view__search :deep(.v-field) {
+  border-radius: 0;
+}
+</style>
