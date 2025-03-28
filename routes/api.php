bc2ff13e77d2db799bba14b08b96ed935998a2ad
Алексей Цапаев <aleksei.tsapaev@yasno.live>
2025-03-29 00:34:11 +0300

    123

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AttributeController;
 use App\Http\Controllers\ContentBlockController;
+use App\Http\Controllers\DataCollectionController;
 use App\Http\Controllers\FileController;
 use App\Http\Controllers\LinkController;
 use App\Http\Controllers\PageController;
@@ -9,26 +10,26 @@ use App\Http\Controllers\TemplateController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->middleware(['auth:sanctum', 'role:root'])->group(function () {
-  Route::get('/me', [UserController::class, 'me']);
+$resources = [
+  'user' => UserController::class,
+  'page' => PageController::class,
+  'template' => TemplateController::class,
+  'link' => LinkController::class,
+  'content-block' => ContentBlockController::class,
+  'attribute' => AttributeController::class,
+  'file' => FileController::class,
+  'data-collection' => DataCollectionController::class,
+];
 
-  Route::apiResources([
-    'user' => UserController::class,
-    'page' => PageController::class,
-    'template' => TemplateController::class,
-    'link'=> LinkController::class,
-    'content-block'=> ContentBlockController::class,
-    'attribute' => AttributeController::class,
-    'file' => FileController::class,
-  ]);
 
-  Route::prefix('destroy')->group(function () {
-    Route::post('user', [UserController::class, 'deleteMany']);
-    Route::post('page', [PageController::class, 'deleteMany']);
-    Route::post('template', [TemplateController::class, 'deleteMany']);
-    Route::post('link', [LinkController::class, 'deleteMany']);
-    Route::post('content-block', [ContentBlockController::class, 'deleteMany']);
-    Route::post('attribute', [AttributeController::class, 'deleteMany']);
-    Route::post('file', [FileController::class, 'deleteMany']);
+Route::prefix('admin')->middleware(['auth:sanctum', 'role:root'])->group(function () use($resources) {
+  Route::get('me', [UserController::class, 'me']);
+
+  Route::apiResources($resources);
+  
+  Route::prefix('destroy')->group(function () use ($resources) {
+    foreach ($resources as $route => $controller) {
+      Route::post($route, [$controller, 'deleteMany']);
+    }
   });
 });
